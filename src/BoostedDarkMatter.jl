2926b6be6ed74e2ba0039e5c1fd1478377466bf9
Chen Xia <xiachen@sjtu.edu.cn>
2023-03-27 23:03:30 +0800

    Add p1_min

--- src/BoostedDarkMatter.jl
+++ src/BoostedDarkMatter.jl
@@ -5,7 +5,8 @@ export
     T4_max,
     T4_max_nonrel,
     T1_min,
-    T1_min_nonrel
+    T1_min_nonrel,
+    p1_min
 
 include("Kinematics.jl")
 using .Kinematics
--- src/Kinematics.jl
+++ src/Kinematics.jl
@@ -16,7 +16,8 @@ export
     T4_max,
     T4_max_nonrel,
     T1_min,
-    T1_min_nonrel
+    T1_min_nonrel,
+    p1_min
 
 using NumericalTools: sqrtm1
 
@@ -78,20 +79,20 @@ stationary target 2 in elastic scattering,
 \begin{aligned}
     T_1^{\min}
 &=
-    \left(\frac{T_4}{2} - m_2\right)
-    \left(1 ± \sqrt{1 + \frac{2T_4}{m_1}\frac{(m_1+m_2)^2}{(2m_2-T_4)^2}}\right)\\
+    \left(\frac{T_4}{2} - m_1\right)
+    \left(1 ± \sqrt{1 + \frac{2T_4}{m_2}\frac{(m_1+m_2)^2}{(2m_1-T_4)^2}}\right)\\
 &=
-    \frac{T_4}{2} - m_2
+    \frac{T_4}{2} - m_1
     +
-    \sqrt{\left(\frac{T_4}{2} - m_2\right)^2 + \frac{T_4(m_1+m_2)^2}{2m_1}}.
+    \sqrt{\left(\frac{T_4}{2} - m_1\right)^2 + \frac{T_4(m_1+m_2)^2}{2m_2}}.
 \end{aligned}
 ```
 
 In the case of
-``T_4 < 2m_2`` and ``\left(\frac{T_4}{2} - m_2\right)^2 ≫ \frac{T_4(m_1+m_2)^2}{2m_1}``,
+``T_4 < 2m_1`` and ``\left(\frac{T_4}{2} - m_1\right)^2 ≫ \frac{T_4(m_1+m_2)^2}{2m_2}``,
 we need to expand the square root to avoid precision loss, and the result is
 ```math
-T_4^{\min} ≈ \frac{T_4(m_1+m_2)^2}{2m_1(2m_2 - T_4)}.
+T_4^{\min} ≈ \frac{T_4(m_1+m_2)^2}{2m_2(2m_1 - T_4)}.
 ```
 
 # Arguments
@@ -103,8 +104,8 @@ T_4^{\min} ≈ \frac{T_4(m_1+m_2)^2}{2m_1(2m_2 - T_4)}.
 """
 function T1_min(T4, m1, m2)
     fac1 = T4 / 2 - m1
-    fac2 = 2T4 / m2 * ((m1 + m2) / (2m2 - T4))^2
-    if fac1 < 0
+    fac2 = 2T4 / m2 * ((m1 + m2) / (2m1 - T4))^2
+    if fac1 < zero(T4)
         return -fac1 * sqrtm1(fac2)
     end
     return fac1 * (1 + sqrt(1 + fac2))
@@ -123,6 +124,20 @@ Non-relativistic version of [`T1_min`](@ref),
 """
 function T1_min_nonrel(T4, m1, m2)
     return m1 * m2 * T4 / (4 * reduce_m(m1, m2)^2)
+end
+
+
+function p1_min(q, ΔE, m1, m2)
+    if ΔE >= q
+        @warn "Kinematics Invalid: ΔE = $ΔE >= q = $q, Inf returned"
+        return Inf
+    end
+    return q / 2 + ΔE / 2 * sqrt(1 + 4m1^2 / ((q + ΔE) * (q - ΔE)))
+end
+
+
+function T1_min(q, ΔE, m1, m2)
+    return m1 * sqrtm1((p1_min(q, ΔE, m1, m2) / m1)^2)
 end
 
 
