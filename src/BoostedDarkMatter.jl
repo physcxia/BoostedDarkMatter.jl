592cde5864594ed202d89003184634e19b577c97
Chen Xia <xiachen@sjtu.edu.cn>
2023-03-28 23:53:53 +0800

    Rename p1_min to p1_min_q

--- src/BoostedDarkMatter.jl
+++ src/BoostedDarkMatter.jl
@@ -6,7 +6,9 @@ export
     T4_max_nonrel,
     T1_min,
     T1_min_nonrel,
-    p1_min
+    p1_min,
+    p1_min_q,
+    T1_min_q
 
 include("Kinematics.jl")
 using .Kinematics
--- src/Kinematics.jl
+++ src/Kinematics.jl
@@ -17,7 +17,8 @@ export
     T4_max_nonrel,
     T1_min,
     T1_min_nonrel,
-    p1_min
+    p1_min_q,
+    T1_min_q
 
 using NumericalTools: sqrtm1
 
@@ -72,8 +73,8 @@ end
 @doc raw"""
     T1_min(T4, m1, m2)
 
-Minimal kinetic energy of particle 1 required to obtain a recoil energy of `T4` for the
-stationary target 2 in elastic scattering,
+Minimal kinetic energy of particle 1 required to produce a recoil energy `T4` of target 2
+at rest in elastic scattering,
 
 ```math
 \begin{aligned}
@@ -94,6 +95,8 @@ we need to expand the square root to avoid precision loss, and the result is
 ```math
 T_4^{\min} ≈ \frac{T_4(m_1+m_2)^2}{2m_2(2m_1 - T_4)}.
 ```
+Here we simply use the function `sqrtm1` provided by
+[`NumericalTools.jl`](https://github.com/physcxia/NumericalTools.jl) to handle this case.
 
 # Arguments
 
@@ -103,12 +106,9 @@ T_4^{\min} ≈ \frac{T_4(m_1+m_2)^2}{2m_2(2m_1 - T_4)}.
 
 """
 function T1_min(T4, m1, m2)
-    fac1 = T4 / 2 - m1
-    fac2 = 2T4 / m2 * ((m1 + m2) / (2m1 - T4))^2
-    if fac1 < zero(T4)
-        return -fac1 * sqrtm1(fac2)
-    end
-    return fac1 * (1 + sqrt(1 + fac2))
+    fac1 = m1 - T4 / 2
+    fac2 = T4 * (m1 + m2)^2 / 2m2
+    return sqrtm1(fac2, fac1)
 end
 
 
@@ -118,8 +118,10 @@ end
 Non-relativistic version of [`T1_min`](@ref),
 
 ```math
-    T_1^{\min} = \frac{m_1 m_2 T_4}{4 μ_{12}^2} = \frac{(m_1 + m_2)^2 T_4}{4m_1 m_2}.
+    T_1^{\min} = \frac{m_1 m_2 T_4}{4 μ_{12}^2} = \frac{(m_1 + m_2)^2 T_4}{4m_1 m_2},
 ```
+
+where ``μ_{12} = m_1 m_2 / (m_1 + m_2)`` is the reduced mass.
 
 """
 function T1_min_nonrel(T4, m1, m2)
@@ -127,17 +129,44 @@ function T1_min_nonrel(T4, m1, m2)
 end
 
 
-function p1_min(q, ΔE, m1, m2)
-    if ΔE >= q
-        @warn "Kinematics Invalid: ΔE = $ΔE >= q = $q, Inf returned"
-        return Inf
+@doc raw"""
+    p1_min_q(q, ΔE, m1)
+
+Minimal momentum of particle 1 with given momentum transfer `q` and energy transfer `ΔE`,
+
+```math
+    p_1^{\min}
+=
+    \frac{q}{2} + \frac{ΔE}{2} \sqrt{1 + \frac{4m_1^2}{q^2 - ΔE^2}}
+```
+
+Note that the mass of the particle 1 `m1` is assumed to be unchanged after the scattering,
+which guarantees ``q > ΔE``.
+
+# Arguments
+
+- `q`: Momentum transfer.
+- `ΔE`: Energy transfer.
+- `m1`: Mass of the incident particle 1.
+
+"""
+function p1_min_q(q, ΔE, m1)
+    if q <= ΔE
+        @warn "Kinematics Invalid: q = $q <= ΔE = $ΔE, Inf returned"
+        return Inf * oneunit(promote_type(typeof(q), typeof(ΔE), typeof(m1)))
     end
     return q / 2 + ΔE / 2 * sqrt(1 + 4m1^2 / ((q + ΔE) * (q - ΔE)))
 end
 
+@doc raw"""
+    T1_min_q(q, ΔE, m1)
 
-function T1_min(q, ΔE, m1, m2)
-    return m1 * sqrtm1((p1_min(q, ΔE, m1, m2) / m1)^2)
+Minimal kinetic energy of particle 1 with given momentum transfer `q`, energy transfer `ΔE`,
+and mass `m1`. It is simply calculated from [`p1_min_q`](@ref) using the on-shell condition.
+
+"""
+function T1_min_q(q, ΔE, m1)
+    return m1 * sqrtm1((p1_min_q(q, ΔE, m1) / m1)^2)
 end
 
 
